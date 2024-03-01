a2ec58f96ea6790694dfafba1605750a2ba8ee9e
17slaterj <160626221+17slaterj@users.noreply.github.com>
2024-03-01 17:40:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ resolution = (1920,1080)
 
 pygame.init()
 window = pygame.display.set_mode(resolution)
+pygame.display.set_caption("Cave game") # Placeholder game name
 clock = pygame.Clock()
 
 
@@ -18,13 +19,33 @@ placeholder_ground_surface.fill((255,0,0))
 placeholder_ground_rectangle = placeholder_ground_surface.get_rect(topleft = (0,900))
 
 # Player
-#player_surface = pygame.image.load()
-player_surface = pygame.Surface((100,100))
-player_surface.fill((0,255,0))
+## Sprites
+player_idle = pygame.transform.scale(pygame.image.load("Assets\Player\playerIdle.png").convert_alpha(), (256,256))
+player_run = (pygame.transform.scale(pygame.image.load("Assets\Player\playerRun1.png").convert_alpha(), (256,256)),
+			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun2.png").convert_alpha(), (256,256)),
+			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun3.png").convert_alpha(), (256,256)),
+			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun4.png").convert_alpha(), (256,256)),
+			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun5.png").convert_alpha(), (256,256)),
+			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun6.png").convert_alpha(), (256,256)),
+			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun7.png").convert_alpha(), (256,256)),
+			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun8.png").convert_alpha(), (256,256)))
+
+## Surface and rectangle
+player_surface = player_idle
 player_rectangle = player_surface.get_rect(center = (resolution[0]/2,resolution[1]/2))
 
 player_vert_vel = 0
-player_vert_acc = 0.3
+player_vert_acc = 0.9
+
+facing_left = False
+facing_right = True
+
+running = -1
+run_count = 0
+
+## Controls
+move_left = False
+move_right = False
 
 
 
@@ -32,10 +53,69 @@ player_vert_acc = 0.3
 while True:
 	# Event loop
 	for event in pygame.event.get():
+		# Key press
+		if event.type == pygame.KEYDOWN:
+			# Jump
+			if event.key == pygame.K_SPACE and player_rectangle.bottom == placeholder_ground_rectangle.top:
+				player_vert_vel = -20 
+			# Move left
+			if event.key == pygame.K_s:
+				move_left = True
+			# Move right
+			if event.key == pygame.K_f:
+				move_right = True
+		
+		# Key release
+		if event.type == pygame.KEYUP:
+			# Move left
+			if event.key == pygame.K_s:
+				move_left = False
+			if event.key == pygame.K_f:
+				move_right = False
+
 		# Quit game
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit()
+	
+	# Player motion
+	## Gravity
+	player_vert_vel += player_vert_acc
+	player_rectangle.y += player_vert_vel
+	if player_rectangle.bottom > placeholder_ground_rectangle.top:
+		player_rectangle.bottom = placeholder_ground_rectangle.top
+		player_vert_vel = 0
+
+	## Movement
+	if move_right == True or move_left == True:
+		run_count += 1
+		if run_count == 6:
+			running += 1
+			player_surface = player_run[running]
+			if running == 7:
+				running = -1
+			run_count = 0
+	
+	if move_left == True:
+		player_rectangle.x -= 5
+		
+		# Face left
+		if facing_left == False:
+			player_surface = pygame.transform.flip(player_surface, True, False)
+			facing_left = True
+			facing_right = False
+
+	if move_right == True:
+		player_rectangle.x += 5
+
+		# Face right
+		if facing_right == False:
+			player_surface = pygame.transform.flip(player_surface, True, False)
+			facing_right = True
+			facing_left = False
+	
+	if move_left == False and move_right == False:
+		player_surface = player_idle
 
 
 	# Render surfaces
@@ -43,14 +123,6 @@ while True:
 	window.blit(placeholder_ground_surface, placeholder_ground_rectangle)
 	window.blit(player_surface, player_rectangle)
 
-
-	# Player physics
-	if player_rectangle.bottom >= placeholder_ground_rectangle.top:
-		player_rectangle.bottom = placeholder_ground_rectangle.top
-		player_vert_vel = 0
-	else:
-		player_vert_vel += player_vert_acc
-		player_rectangle.y += player_vert_vel
 
 
 	# Update display
