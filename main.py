7642f791848b1484ef6b13b19ad0799c1d04fe34
17slaterj <160626221+17slaterj@users.noreply.github.com>
2024-03-03 14:28:59 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -29,10 +29,17 @@ player_run = (pygame.transform.scale(pygame.image.load("Assets\Player\playerRun1
 			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun6.png").convert_alpha(), (256,256)),
 			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun7.png").convert_alpha(), (256,256)),
 			  pygame.transform.scale(pygame.image.load("Assets\Player\playerRun8.png").convert_alpha(), (256,256)))
+player_roll = (pygame.transform.scale(pygame.image.load("Assets\Player\playerRoll1.png").convert_alpha(), (256,256)),
+			   pygame.transform.scale(pygame.image.load("Assets\Player\playerRoll2.png").convert_alpha(), (256,256)),
+			   pygame.transform.scale(pygame.image.load("Assets\Player\playerRoll3.png").convert_alpha(), (256,256)),
+			   pygame.transform.scale(pygame.image.load("Assets\Player\playerRoll4.png").convert_alpha(), (256,256)),
+			   pygame.transform.scale(pygame.image.load("Assets\Player\playerRoll5.png").convert_alpha(), (256,256)),
+			   pygame.transform.scale(pygame.image.load("Assets\Player\playerRoll6.png").convert_alpha(), (256,256)))
 
 ## Surface and rectangle
 player_surface = player_idle
 player_rectangle = player_surface.get_rect(center = (resolution[0]/2,resolution[1]/2))
+face_left = False
 
 player_vert_vel = 0
 player_vert_acc = 0.9
@@ -41,9 +48,15 @@ player_vert_acc = 0.9
 move_left = False
 move_right = False
 
+roll = False
+roll_frames = 30
+
 ## Animation
 run_frame_count = 0
 run_sprite_count = 0
+
+roll_frame_count = 0
+roll_sprite_count = 0
 
 
 
@@ -62,6 +75,9 @@ while True:
 			# Move right
 			if event.key == pygame.K_f:
 				move_right = True
+			# Roll
+			if event.key == pygame.K_BACKSLASH:
+				roll = True
 		
 		# Key release
 		if event.type == pygame.KEYUP:
@@ -85,36 +101,76 @@ while True:
 		player_rectangle.bottom = placeholder_ground_rectangle.top
 		player_vert_vel = 0
 
-	## Movement
-	### Left
-	if move_left == True:
+	## Movement and actions
+	### Roll
+	if roll == True:
+		# Roll left
+		if move_left == True:
+			player_rectangle.x -= 7
+			player_surface = pygame.transform.flip(player_roll[roll_sprite_count], True, False)
+		# Roll right
+		else:
+			player_rectangle.x += 7
+			player_surface = player_roll[roll_sprite_count]
+		
+		# Animation
+		roll_frame_count += 1
+
+		if roll_frame_count == 5:
+			roll_sprite_count += 1
+			roll_frame_count = 0
+		if roll_sprite_count == 6:
+			roll_sprite_count = 0
+		
+		# Roll length
+		roll_frames -= 1
+		if roll_frames == 0:
+			roll = False
+			roll_frames = 30
+		
+
+	
+	### Run left
+	elif move_left == True:
 		# Move left
 		player_rectangle.x -= 5
 
 		# Face left
-		if run_frame_count == 0:
-			player_surface = pygame.transform.flip(player_run[run_sprite_count], True, False)
+		player_surface = pygame.transform.flip(player_run[run_sprite_count], True, False)
 		run_frame_count += 1
 		if run_frame_count == 6:
 			run_sprite_count += 1
 			run_frame_count = 0
 		if run_sprite_count == 8:
 			run_sprite_count = 0
+		
+		face_left = True
 
-	### Right
-	if move_right == True:
+	### Run right
+	elif move_right == True:
 		# Move right
 		player_rectangle.x += 5
 
 		# Face right
-		if run_frame_count == 0:
-			player_surface = player_run[run_sprite_count]
+		player_surface = player_run[run_sprite_count]
 		run_frame_count += 1
 		if run_frame_count == 6:
 			run_sprite_count += 1
 			run_frame_count = 0
 		if run_sprite_count == 8:
 			run_sprite_count = 0
+		
+		face_left = False
+	
+	### Idle
+	if move_left == False and move_right == False:
+		run_frame_count = 0
+		run_sprite_count = 0
+
+		if face_left == True:
+			player_surface = pygame.transform.flip(player_idle, True, False)
+		else:
+			player_surface = player_idle
 
 
 	# Render surfaces
